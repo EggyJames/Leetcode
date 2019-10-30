a7b4c7871aa5a9ab197ebd81c681feb70d5db093
EggyJames <523961500@qq.com>
2019-10-30 21:52:39 +0800

    有序链表转换为二叉搜索树

---
+++ 109.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+        else if(head->next == NULL)
+            return new TreeNode (head->val);
+        ListNode* pre = head;
+        ListNode* p = pre->next;
+        ListNode* q = p->next;
+        while(q!=NULL and q->next !=NULL){
+            pre = pre->next;
+            p = pre->next;
+            q = q->next->next;
+        }
+        pre->next = NULL;
+        TreeNode* root = new TreeNode (p->val);
+        root->left = sortedListToBST(head);
+        root->right=sortedListToBST(p->next);
+        return root;
+    }
+};
